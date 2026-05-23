68acea0b9111d08b0130644dc436bb726140de38
fabioFgc7 <99999716+fabioFgc7@users.noreply.github.com>
2026-05-23 15:33:48 -0300

    corregir error de createGoal

--- backend/src/controllers/goals.controller.ts
+++ backend/src/controllers/goals.controller.ts
@@ -4,20 +4,38 @@ import { prisma } from "../config/prisma.js";
 export const createGoal = async (req: Request, res: Response) => {
   try {
     const userId = req.userId;
+
     if (!userId) {
-      return res.status(401).json({ message: "No autorizado" });
+      return res.status(401).json({
+        message: "No autorizado",
+      });
     }
 
     const { title, month, type, year, targetValue } = req.body;
+
+    console.log("BODY GOAL:", req.body);
+    console.log("USER:", userId);
+
     const goal = await prisma.goal.create({
-      data: { userId: userId, title, month, type, year, targetValue },
+      data: {
+        userId,
+        title,
+        month,
+        type,
+        year,
+        targetValue,
+      },
     });
-    res.status(201).json(goal);
+
+    return res.status(201).json(goal);
   } catch (error) {
-    res.status(500).json({ message: "Error al crear Goal" });
+    console.error("CREATE GOAL ERROR:", error);
+
+    return res.status(500).json({
+      message: "Error al crear Goal",
+    });
   }
 };
-
 export const getGoals = async (req: Request, res: Response) => {
   try {
     const userId = req.userId;
@@ -53,7 +71,7 @@ export const getGoals = async (req: Request, res: Response) => {
 
 export const updateGoal = async (
   req: Request<{ id: string }>,
-  res: Response
+  res: Response,
 ) => {
   try {
     const { id } = req.params;
@@ -74,7 +92,7 @@ export const updateGoal = async (
 };
 export const deleteGoal = async (
   req: Request<{ id: string }>,
-  res: Response
+  res: Response,
 ) => {
   try {
     const { id } = req.params;
@@ -90,7 +108,7 @@ export const deleteGoal = async (
 
 export const updateGoalProgress = async (
   req: Request<{ id: string }>,
-  res: Response
+  res: Response,
 ) => {
   try {
     const { id } = req.params;
