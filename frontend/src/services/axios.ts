39d4a3c0f9c16b4c7c5746350489131e1ed4fe75
fabioFgc7 <99999716+fabioFgc7@users.noreply.github.com>
2026-05-22 21:11:29 -0300

    remove dotenv/config

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
 });
+console.log(import.meta.env.VITE_API_URL);
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
